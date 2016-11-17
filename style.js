e0f554459761d67b9fdcba00d00832377a8cf8d4
Bony5690 <jerrybony@yahoo.com>
2016-11-17 06:15:49 -0600

    Periodic Table

---
+++ style.js
@@ -0,0 +1,19 @@
+$(".style1").css("background-color", "#ffaa00");
+
+$(".style2").css("background-color", "#f3f300");
+
+$(".style3").css("background-color", "#dd9999");
+
+$(".style4").css("background-color", "#11ddbb");
+
+// div.classList.remove("foo");
+
+$(".style5").css("background-color", "#00ee00");
+
+$(".style6").css("background-color", "#88aaff");
+
+$(".number").css("background-color", "#f4dcd4");
+
+$(".style7").css("background-color", "#e7dad4");
+
+$(".style8").css("background-color", "#f4dcd4");
